6e277399e002b1337f9cde847444a05782461bf8
Roberto Achar <robertoachar@gmail.com>
2019-11-13 14:53:32 -0300

    feature: add koa custom logger

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
-import logger from 'koa-logger';
+import koaLogger from 'koa-logger';
 import helmet from 'koa-helmet';
 
 import error from './error';
+import logger from './logger';
 import router from './router';
 
 const app = new Koa();
 app.use(helmet());
 app.use(bodyParser());
 app.use(cors());
-app.use(logger());
+app.use(koaLogger(str => logger.info(str)));
 app.use(error);
 app.use(router.routes());
 app.use(router.allowedMethods());
